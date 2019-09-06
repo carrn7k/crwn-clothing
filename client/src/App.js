32034d1771101e22f1f5b8ee8b49ac93be918cf2
Heath Carr <carrn7k@gmail.com>
2019-09-06 16:14:07 +0900

    converted some scss styles to styled components

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -11,6 +10,7 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import Header from './components/header/header.component';
 import CheckoutPage from './pages/checkout/checkout.component';
 
+import { GlobalStyle } from './global.styles';
 
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
@@ -24,6 +24,7 @@ const App = ({ checkUserSession, currentUser }) => {
 
   return (
     <div>
+      <GlobalStyle />
     	<Header />
     	<Switch>
 	      <Route exact path='/' component={HomePage} />
--- client/src/components/collection-preview/collection-preview.component.jsx
+++ client/src/components/collection-preview/collection-preview.component.jsx
@@ -3,19 +3,25 @@ import React from 'react';
 
 import CollectionItem from '../collection-item/collection-item.component';
 
+import {
+  CollectionsPreviewContainer,
+  TitleContainer,
+  PreviewContainer
+} from './collection-preview.styles';
+
 import './collection-preview.styles.scss';
 
 const CollectionPreview = ({ title, items }) => (
-  <div className='collection-preview'>
-    <h1 className='title'>{title.toUpperCase()}</h1>
-    <div className='preview'>
+  <CollectionsPreviewContainer>
+    <TitleContainer>{title.toUpperCase()}</TitleContainer>
+    <PreviewContainer>
       {items
         .filter((item, idx) => idx < 4)
         .map(item => (
           <CollectionItem key={item.id} item={item} />
         ))}
-    </div>
-  </div>
+    </PreviewContainer>
+  </CollectionsPreviewContainer>
 );
 
 export default CollectionPreview;
